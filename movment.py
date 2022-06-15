32fabeea3aae696111eafe1318418bc3be745abc
yonifrach <107173434+yonifrach@users.noreply.github.com>
2022-06-15 23:33:14 +0300

    Update movment.py
    fix to y_movement

--- movment.py
+++ movment.py
@@ -10,35 +10,35 @@ def y_movement(dire, locX , locY, borderX , borderY):
 x=150
 y=60
 print(x,y,'L')
-y_movment('L',x,y,BORDERX,HEIGHT)
+y_movement('L',x,y,BORDERX,HEIGHT)
 x=0
 y=60
 print(x,y,'L')
-y_movment('L',x,y,BORDERX,HEIGHT)
+y_movement('L',x,y,BORDERX,HEIGHT)
 
 x=385
 y=160
 print(x,y,'R')
-movment('R',x,y,BORDERX,HEIGHT)
+y_movement('R',x,y,BORDERX,HEIGHT)
 x=200
 y=160
 print(x,y,'R')
-movment('R',x,y,BORDERX,HEIGHT)
+y_movement('R',x,y,BORDERX,HEIGHT)
 
 x=20
 y=0
 print(x,y,'U')
-y_movment('U',x,y,BORDERX,HEIGHT)
+y_movement('U',x,y,BORDERX,HEIGHT)
 x=20
 y=100
 print(x,y,'U')
-y_movment('U',x,y,BORDERX,HEIGHT)
+y_movement('U',x,y,BORDERX,HEIGHT)
 
 x=400
 y=460
 print(x,y,'D')
-movment('R',x,y,BORDERX,HEIGHT)
+y_movement('R',x,y,BORDERX,HEIGHT)
 x=400
 y=160
 print(x,y,'D')
-movment('R',x,y,BORDERX,HEIGHT)
+y_movement('R',x,y,BORDERX,HEIGHT)
